696a650162bca8a5848c3085607764fe2ebce250
dudezi <a01036952858@gmail.com>
2024-09-10 09:57:01 +0900

    chore:테스트findAll

--- src/test/java/com/example/sbb4/Sbb4ApplicationTests.java
+++ src/test/java/com/example/sbb4/Sbb4ApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.sbb4;
 
+import com.example.sbb4.question.Question;
+import com.example.sbb4.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Sbb4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
 
+	@Test
+	void testJpa() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
 }
