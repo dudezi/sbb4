448665896d016738f94f69e8b1b86d4b6fa21b15
dudezi <a01036952858@gmail.com>
2024-09-10 11:28:15 +0900

    feat:list추가중

--- src/main/java/com/example/sbb4/question/QuestionController.java
+++ src/main/java/com/example/sbb4/question/QuestionController.java
@@ -1,7 +1,17 @@
 package com.example.sbb4.question;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public Question questionList() {
+        Question q = new Question();
+        return q;
+    }
+
 }
