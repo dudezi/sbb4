100cdb406fdcc4bdc76d27de5a1a940e12ac6d00
dudezi <a01036952858@gmail.com>
2024-09-10 13:13:44 +0900

    ãfeat:질문목록1차완료

--- src/main/java/com/example/sbb4/question/QuestionController.java
+++ src/main/java/com/example/sbb4/question/QuestionController.java
@@ -1,17 +1,22 @@
 package com.example.sbb4.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    @ResponseBody
-    public Question questionList() {
-        Question q = new Question();
-        return q;
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 
 }
--- src/test/java/com/example/sbb4/Sbb4ApplicationTests.java
+++ src/test/java/com/example/sbb4/Sbb4ApplicationTests.java
@@ -102,16 +102,25 @@ class Sbb4ApplicationTests {
 //		assertEquals(2, a.getQuestion().getId());
 //	}
 
-	@Transactional
+//	@Transactional
+//	@Test
+//	void testJpa10() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		List<Answer> answerList = q.getAnswerList();
+//
+//		assertEquals(1, answerList.size());
+//		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//	}
+
 	@Test
-	void testJpa10() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
 	}
 }
